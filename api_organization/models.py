4d2648211b3634850f9a72063835e39f8accd999
Vlad <pu4kovvlad@gmail.com>
2017-09-10 23:29:21 +0300

    Изменил размещение моделей в приложениях и сделал миграции

--- api_organization/models.py
+++ api_organization/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from api_partners.models import Form
 
 #Модель кредитной организации
 class Credit_organization(models.Model):
@@ -23,18 +24,6 @@ class Proposal(models.Model):
     def __str__(self):
         return str(self.name)
 
-#Модель анкеты клиента
-class Form(models.Model):
-    date_of_creation = models.DateTimeField('Дата и время создания', auto_now_add=True)
-    date_of_change = models.DateTimeField('Дата и время изменения', auto_now=True)
-    full_name = models.CharField('ФИО', max_length=250)
-    dob = models.DateField('Дата рождения')
-    phone_number = models.CharField('Телефон', max_length=50)
-    passport = models.CharField('Паспорт', max_length=100)
-    scoring_ball = models.IntegerField('Скоринговый балл')
-
-    def __str__(self):
-        return str(self.full_name)
 
 # Модель для выпадающего списка статусов
 class Status_bid(models.Model):
--- api_partners/models.py
+++ api_partners/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#Модель анкеты клиента
+class Form(models.Model):
+    date_of_creation = models.DateTimeField('Дата и время создания', auto_now_add=True)
+    date_of_change = models.DateTimeField('Дата и время изменения', auto_now=True)
+    full_name = models.CharField('ФИО', max_length=250)
+    dob = models.DateField('Дата рождения')
+    phone_number = models.CharField('Телефон', max_length=50)
+    passport = models.CharField('Паспорт', max_length=100)
+    scoring_ball = models.IntegerField('Скоринговый балл')
+
+    def __str__(self):
+        return str(self.full_name)
